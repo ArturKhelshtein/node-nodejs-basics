434d7eb03732e09b73f49b4005136019fb9c35a5
Artur Khelshtein <artur.khelshtein@live.com>
2025-04-23 18:43:38 +0300

    feat: rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import path from 'path';
+import { promises } from 'fs';
+import { fileURLToPath } from 'url';
+
 const rename = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'wrongFilename.txt');
+    const renameFilePath = path.join(__dirname, 'files', 'properFilename.md');
+
+    try {
+        await promises.access(renameFilePath);
+        throw new Error('FS operation failed, already exist');
+    } catch (error) {
+        if (error.code !== 'ENOENT') {
+            throw new Error('FS operation failed, unexpected');
+        }
+    }
+
+    try {
+        await promises.access(filePath);
+        await promises.rename(filePath, renameFilePath);
+    } catch (error) {
+        throw new Error('FS operation failed, source file missed');
+    }
 };
 
 await rename();
