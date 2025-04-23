8c6f0eb2966229144e8b72d85263cda1ffb802ba
Artur Khelshtein <artur.khelshtein@live.com>
2025-04-23 19:06:12 +0300

    feat: delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import path from 'path';
+import { promises } from 'fs';
+import { fileURLToPath } from 'url';
+
 const remove = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'fileToRemove.txt');
+
+    try {
+        await promises.unlink(filePath);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed, the file is already deleted');
+        }
+        throw new Error('FS operation failed, unexpected');
+    }
 };
 
 await remove();
