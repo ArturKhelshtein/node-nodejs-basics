c116a69ce4fce88e516b1eaa376e2b60ff83bf8f
Artur Khelshtein <artur.khelshtein@live.com>
2025-04-24 21:25:58 +0300

    feat: calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const hash = createHash('sha256');
+    
+    return new Promise((resolve, reject) => {
+        const stream = createReadStream(filePath);
+        
+        stream.on('error', reject);
+        stream.on('data', (chunk) => hash.update(chunk));
+        stream.on('end', () => {
+            const hexHash = hash.digest('hex');
+            console.log(hexHash);
+            resolve(hexHash);
+        });
+    });
 };
 
 await calculateHash();
