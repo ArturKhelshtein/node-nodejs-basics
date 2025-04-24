1caa20cc492930f6dfca8f367f3c57fedcda9fbd
Artur Khelshtein <artur.khelshtein@live.com>
2025-04-24 22:12:49 +0300

    fix: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,24 +1,21 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { createReadStream } from 'fs';
+import { readFile } from 'node:fs/promises';
 import { createHash } from 'crypto';
 
 const calculateHash = async () => {
     const __dirname = path.dirname(fileURLToPath(import.meta.url));
-    const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
-    const hash = createHash('sha256');
+    try {
+        const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+        const fileContent = await readFile(filePath);
+        const hash = createHash('sha256');
+        hash.update(fileContent);
+        const hexHash = hash.digest('hex');
+        console.log(hexHash);
+    } catch (error) {
+        throw new Error('Operation failed');
+    }
     
-    return new Promise((resolve, reject) => {
-        const stream = createReadStream(filePath);
-        
-        stream.on('error', reject);
-        stream.on('data', (chunk) => hash.update(chunk));
-        stream.on('end', () => {
-            const hexHash = hash.digest('hex');
-            console.log(hexHash);
-            resolve(hexHash);
-        });
-    });
 };
 
 await calculateHash();
