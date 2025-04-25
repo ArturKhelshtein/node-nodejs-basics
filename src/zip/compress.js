852502958afbbec27805fe5c7b65f6c77cdf64f9
Artur Khelshtein <artur.khelshtein@live.com>
2025-04-25 11:14:06 +0300

    feat: compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream, createWriteStream, } from 'fs';
+import process from 'node:process';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+
 const compress = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const srcDir = createReadStream(path.join(__dirname, 'files', 'fileToCompress.txt'));
+    const destDir = createWriteStream(path.join(__dirname, 'files', 'archive.gz'));
+    const gzip = createGzip();
+    
+    pipeline(srcDir, gzip, destDir, (err) => {
+        if (err) {
+          console.error('An error occurred:', err);
+          process.exitCode = 1;
+        }
+      });
 };
 
 await compress();
